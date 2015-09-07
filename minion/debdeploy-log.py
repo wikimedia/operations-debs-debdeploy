221e1ab2ffc317e1de4e1f28da9f1b565206d156
Moritz Muehlenhoff <mmuhlenhoff@wikimedia.org>
2015-09-07 16:29:35 +0200

    Add a Salt returner for local logging
    Also add a workaround for Salt bug writing out it's return status to stdout
    
    Change-Id: I7e61b64afb1c6b43a22fa3513ab93ff9e04fb26d

---
+++ minion/debdeploy-log.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+This returner logs the package status update to /var/log/debdeploy.log
+'''
+
+import datetime
+
+def returner(ret):
+    with open("/var/log/debdeploy.log", "a") as log:
+        indented = [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + l for l in ret['return']['aptlog'].splitlines()]
+        log.write("\n".join(indented))
+
+# Local variables:
+# mode: python
+# End:
